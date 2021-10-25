0e00a94dffff8b7d905c4a8c39fa68ea1e90e2d5
colinZou <313919096@qq.com>
2021-10-26 01:23:02 +0800

    'init'

---
+++ src/test/java/com/colin/manage/EhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colin.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
